7dfb06f1485d6f4d1e7727c00c82ce3fb2953d87
Aetra <vava-hajek@hotmail.fr>
2020-05-06 09:05:40 +0200

    lancement du processus de traitement et méthode de récupération et de renvoie

---
+++ Searcher.py
@@ -0,0 +1,42 @@
+from _planning.killerList import getAllInfo, researchForSearcher
+from _planning.CleanProcess import isFull, is_partly_completed
+from _rules.InfoEnq import getText, getNbVictims, getLieu, getDates, getDateVic
+
+#Resitution des informations pour pouvoir les afficher dans l'ordre victime, date début, date fin, le lieux. Ce qui constitue une liste d'informations sur les tueurs
+def postAllInfo():
+    infoKiller = getAllInfo()
+    all_text = getText(True)
+    tabForSearcher = []
+
+    for containInfo in infoKiller:
+        tabInfo = researchForSearcher(containInfo[1])
+        tabForSearcher.append(tabInfo)
+    for i, text in enumerate(all_text):
+        victims = getNbVictims(text)
+        places = getLieu(text)
+        dates= getDates(text)
+        tabInfo = getDateVic(text)
+        if len(tabForSearcher[i][0]) == 0:
+            tabForSearcher[i][0].append(text[0])
+        if len(victims) > 0 and victims != None:
+            tabForSearcher[i][1].append(victims)
+        if len(places) > 0 and places != None:
+            tabForSearcher[i][2].append(places)
+        if len(dates) > 0 and dates != None:
+            tabForSearcher[i][5].append(dates)
+        if len(tabInfo) > 0 and tabInfo != None:
+            tabForSearcher[i][6].append(tabInfo)
+
+    for killer in tabForSearcher:
+        print(killer)
+    return tabForSearcher
+
+#On renvoie l'ensemble des informations dans un fichier txt, que l'on crée s'il n'existe pas
+resultEnquete=postAllInfo()
+with open("resultEnquete.txt", "w", encoding = 'utf-8') as f:
+    for r in resultEnquete:
+        f.write(str(r) +"\n")
+
+
+postAllInfo()
+
